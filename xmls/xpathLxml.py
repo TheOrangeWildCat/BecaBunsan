543791a7008742ba474ff9144b853d2c1c117357
Angel Acuña <acuhangel@gmail.com>
2022-02-24 11:39:32 -0600

    v2

---
+++ xmls/xpathLxml.py
@@ -0,0 +1,17 @@
+from lxml import etree
+import sys
+
+
+def resultado(tree,query):
+    r = tree.xpath(query)
+    for libro in (r):
+        print(libro.text)
+
+def main():
+    rutaXml = str(sys.argv[1])
+    query = str(sys.argv[2])
+    tree = etree.parse(rutaXml)
+    resultado(tree, query)
+
+if __name__ == "__main__":
+    main()
