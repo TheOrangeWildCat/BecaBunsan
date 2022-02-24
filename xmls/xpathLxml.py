94a1f4e3d710d6cb5ce4206619252552af0d10c1
Angel Acuña <acuhangel@gmail.com>
2022-02-24 11:40:06 -0600

    v2 uso de libreria lxml

--- xmls/xpathLxml.py
+++ xmls/xpathLxml.py
@@ -1,12 +1,11 @@
 from lxml import etree
 import sys
 
-
 def resultado(tree,query):
     r = tree.xpath(query)
     for libro in (r):
         print(libro.text)
-
+        
 def main():
     rutaXml = str(sys.argv[1])
     query = str(sys.argv[2])
