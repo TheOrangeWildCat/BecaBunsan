26db2acfb480d7b7582a225e950cc4fe8ea6d82a
Angel_Acuña <angel.huerta@bunsan.io>
2022-03-04 12:14:08 -0600

    first commit

---
+++ Elixir/becarios_bunsan_2020_b1/lib/becarios_bunsan2020_b1.ex
@@ -0,0 +1,18 @@
+defmodule BecariosBunsan2020B1 do
+  @moduledoc """
+  Documentation for `BecariosBunsan2020B1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BecariosBunsan2020B1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ Elixir/becarios_bunsan_2020_b1/test/becarios_bunsan2020_b1_test.exs
@@ -0,0 +1,8 @@
+defmodule BecariosBunsan2020B1Test do
+  use ExUnit.Case
+  doctest BecariosBunsan2020B1
+
+  test "greets the world" do
+    assert BecariosBunsan2020B1.hello() == :world
+  end
+end
