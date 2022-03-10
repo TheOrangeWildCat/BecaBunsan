dd9fd6857402367711d718b8085d7ab7667160dc
Angel_Acuña <angel.huerta@bunsan.io>
2022-03-09 19:15:12 -0600

    Etapa 1

--- becarios_bunsan_2022_b1/lib/metro_cdmx_challenge.ex
+++ becarios_bunsan_2022_b1/lib/metro_cdmx_challenge.ex
@@ -9,59 +9,90 @@ defmodule MetroCDMXChallenge do
       donde nos mostrara el nombre de la linea y sus estaciones
 
   """
+  @doc """
+
+  Obtains ....
+
+  ## Examples
+    iex> MetroCdmxChallenge.metro_lines("./data/tiny_metro.kml")
+    [
+      %{name: "Línea 5", stations:
+        [
+          %{name: "Pantitlan", coords: "90.0123113 30.012121"},
+          %{name: "Hangares", coords: "90.0123463 30.012158"},
+        ]
+      },
+      %{name: "Línea 3", stations:
+        [
+          %{name: "Universidad", coords: "90.0123113 30.012121"},
+          %{name: "Copilco", coords: "90.0123463 30.012158"},
+        ]
+      }
+    ]
+  """
 
   import SweetXml
+
+  defmodule Line do
+    #defstruct name: "", stations: []
+    defstruct [:name, :stations]
+  end
+
+  defmodule Station do
+    defstruct [:name, :coords]
+  end
+
+
+
 @doc """
 metro_lines( ruta )
 muestra una lista de las lineas con sus estaciones
 """
+
   def metro_lines(xml_path) do
     doc = File.read!(xml_path)
     estaciones = doc |> toDict()
-    _lineas = doc |> metroLineas(estaciones)
+    lineas = doc |> metroLineas(estaciones)
   end
 
   def metro_graph(xml_path) do
-    metro_lines(xml_path)
+    lineas = metro_lines(xml_path)
+
+  end
+
+  defp read(xml_path) do
+    File.read!(xml_path)
   end
 
   defp toDict(doc) do
     estacion = doc |> xpath(~x"//Document/Folder[2]/Placemark/name/text()"ls)
     coordenadas = doc |> xpath(~x"//Document/Folder[2]/Placemark/Point/coordinates/text()"ls)
-    coordenadas =  coordenadasToList(coordenadas)
+    coordenadas =  coordenadas
+    |> Enum.map(fn x ->  x
+    |> String.replace(" ","")
+    |> String.trim()
+    end)
     Enum.zip(coordenadas, estacion) |> Map.new()
   end
 
   defp metroLineas(doc, estaciones) do
     linea = doc |> xpath(~x"//Document/Folder[1]/Placemark/name/text()"ls)
     coordenadas = doc |> xpath(~x"//Document/Folder/Placemark/LineString/coordinates/text()"ls)
-    coordenadas =  coordenadasToList2(coordenadas, estaciones)
-    Enum.zip(linea, coordenadas) |> Map.new()
-  end
-
-  defp coordenadasToList(coordenadas) do
-    coordenadas
+    coordenadas =  coordenadas
     |> Enum.map(fn x ->  x
-    |> String.replace(" ","")
     |> String.trim()
+    |> String.replace(" ","")
+    |> String.split("\n")
+    |> Enum.map(fn x -> {estaciones[x],x} end ) end)
+    Enum.zip(linea, coordenadas) |> Enum.map(fn {lin,stations} ->
+      %{
+        line: lin,
+        stations: Enum.map(stations, fn {name,coords} -> %{name: name, coords: coords} end)
+      }
     end)
-  end
-
-  defp coordenadasToList2(coordenadas, estaciones) do
-      coordenadas
-      |> Enum.map(fn x ->  x
-      |> String.trim()
-      |> String.replace(" ","")
-      |> String.split("\n")
-      |>match(estaciones)  end)
-  end
-
-  def match(lista, estaciones) do
-    lista
-    |> Enum.map(fn x -> {estaciones[x],x} end )
   end
 
 end
 
 # MetroCDMXChallenge.metro_lines("./lib/MetroCDMX.xml")
-# MetroCDMXChallenge.metro_graph
+# MetroCDMXChallenge.metro_graph("./lib/MetroCDMX.xml")
