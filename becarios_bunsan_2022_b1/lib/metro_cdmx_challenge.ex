0614ff22b5ddf28b5ef5cddfb32778a9895e0bc4
Angel_Acuña <angel.huerta@bunsan.io>
2022-03-09 23:38:54 -0600

    Etapa 1/ Structs aplicados/ grafo completado

--- becarios_bunsan_2022_b1/lib/metro_cdmx_challenge.ex
+++ becarios_bunsan_2022_b1/lib/metro_cdmx_challenge.ex
@@ -1,4 +1,13 @@
 defmodule MetroCDMXChallenge do
+  @moduledoc """
+  para obtener la lista de lineas con sus estaciones:
+
+    MetroCDMXChallenge.metro_lines("./lib/MetroCDMX.xml")
+
+  para crear el grafo de las lineas del metro de la ciudad
+
+    MetroCDMXChallenge.metro_graph("./lib/MetroCDMX.xml")
+  """
   import SweetXml
 
   defmodule Line do
@@ -9,12 +18,20 @@ defmodule MetroCDMXChallenge do
     defstruct [:name, :coords]
   end
 
+@doc """
+  metro_lines("ruta")
+  funcion que despliega las lineas del metro de la CDMX con sus respectivas
+  estaciones
+"""
   def metro_lines(xml_path) do
     doc = File.read!(xml_path)
     estaciones = doc |> toDict()
     lineas = doc |> metroLineas(estaciones)
   end
-
+@doc """
+  metro_graph
+  esta funcion crea un grafo con las estaciones de las lineas del metro
+"""
   def metro_graph(xml_path) do
     doc = read(xml_path)
     estaciones = doc |> toDict()
@@ -24,10 +41,18 @@ defmodule MetroCDMXChallenge do
     [_ | l2] = linea
     Enum.zip(linea,l2)
     end)
-    Enum.reduce(listaNodos,graph, fn a, graph -> graph |> Graph.add_edges(a)end)
+    Enum.reduce(listaNodos,graph, fn a, graph ->
+      graph |> Graph.add_edges(a)
+    end)
   end
+@doc """
+  soloLineas(archivo.xml, mapaDeEstaciones)
 
-  def soloLineas(doc, estaciones) do
+  obtiene una lista limpia de las estaciones de cada linea de manera ordenada
+  de acuerdo a su secuencia en la linea
+  entrega la secuencia de los nombres de las estaciones
+"""
+  defp soloLineas(doc, estaciones) do
     doc
     |> xpath(~x"//Document/Folder/Placemark/LineString/coordinates/text()"ls)
     |> Enum.map(fn x ->  x
@@ -40,7 +65,9 @@ defmodule MetroCDMXChallenge do
   defp read(xml_path) do
     File.read!(xml_path)
   end
-
+@doc """
+  crea un diccionario/ mapa %{cooordenadas, nombreDeLaEstación}
+"""
   defp toDict(doc) do
     estacion = doc |> xpath(~x"//Document/Folder[2]/Placemark/name/text()"ls)
     coordenadas = doc |> xpath(~x"//Document/Folder[2]/Placemark/Point/coordinates/text()"ls)
@@ -51,7 +78,10 @@ defmodule MetroCDMXChallenge do
     end)
     Enum.zip(coordenadas, estacion) |> Map.new()
   end
-
+@doc """
+  metroLineas(archivo.xml, MapaDeEstaciones)
+  genera una lista de diccionarios por linea de acuerdo al Struct.Line
+"""
   defp metroLineas(doc, estaciones) do
     linea = doc |> xpath(~x"//Document/Folder[1]/Placemark/name/text()"ls)
     coordenadas = doc
@@ -62,14 +92,12 @@ defmodule MetroCDMXChallenge do
     |> String.split("\n")
     |> Enum.map(fn x -> {estaciones[x],x} end ) end)
     Enum.zip(linea, coordenadas) |> Enum.map(fn {lin,stations} ->
-      %{
-        line: lin,
-        stations: Enum.map(stations, fn {name,coords} -> %{name: name, coords: coords} end)
+      %Line{
+        name: lin,
+        stations: Enum.map(stations, fn {name,coords} ->
+          %Station{name: name, coords: coords}
+        end)
       }
     end)
   end
-
 end
-
-# MetroCDMXChallenge.metro_lines("./lib/MetroCDMX.xml")
-# MetroCDMXChallenge.metro_graph("./lib/MetroCDMX.xml")
