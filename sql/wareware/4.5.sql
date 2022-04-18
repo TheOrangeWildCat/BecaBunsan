2e3901c1a56f2824b86a53a1452d04b30ea7eefa
Angel Acuña <acuhangel@gmail.com>
2022-04-17 22:32:47 -0500

    SQL wareware

--- sql/wareware/4.5.sql
+++ sql/wareware/4.5.sql
@@ -1,8 +1,16 @@
-select (count(*) * 8 * 55) as Worked_hours
+select (count(*) * 8 * (select count(*) 
+    from assignments
+    WHERE (DATE '2022-03-01', DATE '2022-03-31') 
+    OVERLAPS (start_date, end_date))) as Worked_hours
 from  (
     select dd, extract(DOW from dd) as dw
-    from generate_series('2022-03-01'::date, '2022-03-31'::date, '1 day'::interval) dd
-) as month
+    from generate_series('2022-03-01'::date, '2022-03-31'::date, '1 day'::interval) dd) as month
 where  dw not in (6,0);
+
+
+
+
+
+
 
 
